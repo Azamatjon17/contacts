cd13d42338022e7c621b6249b787442b221003d2
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-06-06 13:30:58 +0100

    wed

---
+++ lib/controllers/contacts_controlers.dart
@@ -0,0 +1,27 @@
+import 'package:contacts/models/contact.dart';
+import 'package:contacts/service/local_contact_service.dart';
+
+class ContactsControlers {
+  LocalContactService localContactService = LocalContactService();
+
+  Future<List<Contact>> getContacts() async {
+    List<Contact> newContacts = [];
+    List<Map<String, dynamic>> datas = await localContactService.getContacts();
+    for (Map<String, dynamic> data in datas) {
+      newContacts.add(Contact(id: data['id'], name: data['name'], phone: data['phone']));
+    }
+    return newContacts;
+  }
+
+  Future<void> deleteContact(Contact contact) async {
+    await localContactService.deleteContact(contact.id);
+  }
+
+  Future<void> editContact(Contact contact) async {
+    await localContactService.editContact(contact);
+  }
+
+  Future<void> addContact(Contact contact) async {
+    await localContactService.addContacts(contact);
+  }
+}
